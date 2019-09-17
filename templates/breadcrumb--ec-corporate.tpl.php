52f2623d046568ee9b0ecb8a3f6b097cd5950d68
Tekrane Julien <julien.tekrane@laposte.net>
2019-09-17 14:02:57 +0000

    Breadcrumb: respect DG recommandation (not according with but no choice)

--- templates/breadcrumb--ec-corporate.tpl.php
+++ templates/breadcrumb--ec-corporate.tpl.php
@@ -10,18 +10,18 @@
     <nav class="ecl-page-header__breadcrumb ecl-breadcrumb" aria-label="<?php print t('You are here:'); ?>" data-ecl-breadcrumb="true">
       <ol class="ecl-breadcrumb__container">
         <?php foreach($breadcrumb as $value): ?>
-          <li class="ecl-breadcrumb__segment" data-ecl-breadcrumb-item="static" aria-hidden="false">
-            <?php if($i < $count - 1): ?>
+          <?php if($i < $count - 1): ?>
+            <li class="ecl-breadcrumb__segment" data-ecl-breadcrumb-item="static" aria-hidden="false">
               <?php print l($value['content'], $value['url'], $value['options']); ?>
               <svg focusable="false" aria-hidden="true"
                 role="presentation" class="ecl-breadcrumb__icon ecl-icon ecl-icon--xs ecl-icon--rotate-90">
                 <use xlink:href="/component-library/dist/media/icons.a11fa655.svg#ui--corner-arrow"></use>
               </svg>
+            </li>
             <?php else: ?>
-              <?php // Change ECLv2.11: Adding link adapted to website usages. ?>
-              <li class="ecl-breadcrumb__segment" data-ecl-breadcrumb-item="static" aria-hidden="false"><?php print l($value['content'], $value['url'], $value['options']); ?></li>
+              <?php // Last element is the current. ?>
+              <li class="ecl-breadcrumb__segment ecl-breadcrumb__current-page" aria-current="page" data-ecl-breadcrumb-item="static" aria-hidden="false"><?php print $value['content']; ?></li>
             <?php endif; ?>
-          </li>
           <?php $i++; ?>
         <?php endforeach; ?>
       </ol>
