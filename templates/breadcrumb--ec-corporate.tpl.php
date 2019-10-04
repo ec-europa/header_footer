123ef7cfce9e3a0aa7834a18d3a18f7df5dac105
Tekrane Julien <julien.tekrane@laposte.net>
2019-10-04 09:00:46 +0000

    WEBTOOLS-9440: Undefined index (code review)

--- templates/breadcrumb--ec-corporate.tpl.php
+++ templates/breadcrumb--ec-corporate.tpl.php
@@ -13,7 +13,8 @@ use HeaderFooterCorporate\HeaderFooterCorporateHelpers;
     <nav class="ecl-page-header__breadcrumb ecl-breadcrumb" aria-label="<?php print \t('You are here:'); ?>" data-ecl-breadcrumb="true">
       <ol class="ecl-breadcrumb__container">
         <?php foreach ($breadcrumb as $value): ?>
-          <?php if ($i < $count - 1): ?>
+          <?php if (next($breadcrumb) !== FALSE): ?>
+            <?php // Parent path. ?>
             <li class="ecl-breadcrumb__segment" data-ecl-breadcrumb-item="static" aria-hidden="false">
               <?php print \l($value['content'], $value['url'], $value['options']); ?>
               <svg focusable="false" aria-hidden="true"
@@ -21,12 +22,10 @@ use HeaderFooterCorporate\HeaderFooterCorporateHelpers;
                 <use xlink:href="<?php print HeaderFooterCorporateHelpers::getAssetsPath(); ?>/images/icons/sprites/icons.svg#ui--corner-arrow"></use>
               </svg>
             </li>
-            <?php else: ?>
-              <?php
-// Last element is the current.?>
-              <li class="ecl-breadcrumb__segment ecl-breadcrumb__current-page" aria-current="page" data-ecl-breadcrumb-item="static" aria-hidden="false"><?php print $value['content']; ?></li>
-            <?php endif; ?>
-          <?php $i++; ?>
+          <?php else: ?>
+            <?php // Last element. ?>
+            <li class="ecl-breadcrumb__segment ecl-breadcrumb__current-page" aria-current="page" data-ecl-breadcrumb-item="static" aria-hidden="false"><?php print $value['content']; ?></li>
+          <?php endif; ?>
         <?php endforeach; ?>
       </ol>
     </nav>
