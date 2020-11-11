5b4cc13e12a0041f2f69d6acb54e74a352eac045
serotonine <Julie.DANJOU@ext.ec.europa.eu>
2020-11-11 10:28:34 +0000

    AGM-224 replace hook_preprocess_HOOK by hook_easy_breadcrumb_breadcrumb_alter()

--- templates/breadcrumb-ec-corporate.tpl.php
+++ templates/breadcrumb-ec-corporate.tpl.php
@@ -13,7 +13,7 @@ use HeaderFooterCorporate\HeaderFooterCorporateHelpers;
     <?php foreach ($breadcrumb as $value): ?>
       <?php if ($i < $count - 1): ?>
         <li class="ecl-breadcrumb__segment" data-ecl-breadcrumb-item="static" aria-hidden="false">
-          <?php print \l($value['content'], $value['url'], $value['options']); ?>
+          <?php print $value; ?>
           <svg focusable="false" aria-hidden="true"
             role="presentation" class="ecl-breadcrumb__icon ecl-icon ecl-icon--xs ecl-icon--rotate-90">
             <use xlink:href="<?php print HeaderFooterCorporateHelpers::getAssetsPath(); ?>/images/icons/sprites/icons.svg#ui--corner-arrow"></use>
@@ -21,10 +21,10 @@ use HeaderFooterCorporate\HeaderFooterCorporateHelpers;
         </li>
         <?php else: ?>
           <?php // Last element is the current. ?>
-          <li class="ecl-breadcrumb__segment ecl-breadcrumb__current-page" aria-current="page" data-ecl-breadcrumb-item="static" aria-hidden="false"><?php print $value['content']; ?></li>
+          <li class="ecl-breadcrumb__segment ecl-breadcrumb__current-page" aria-current="page" data-ecl-breadcrumb-item="static" aria-hidden="false"><?php print render($value); ?></li>
         <?php endif; ?>
       <?php $i++; ?>
     <?php endforeach; ?>
   </ol>
 </nav>
-<h1 class="ecl-page-header__title" aria-current="page"><?php print $page_title; ?></h1>
+<div class="ecl-page-header__title"><?php print $site_name; ?></div>
